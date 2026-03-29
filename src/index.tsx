85fa4480b84523dd529c600e835d624f3f0658d7
korn33 <korn33.mail@gmail.com>
2026-03-29 18:10:49 +0300

    ресет стилей

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App/App";
+import './reset.module.scss'
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element not found");
